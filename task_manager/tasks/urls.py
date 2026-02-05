2b0aac9b04c410e6d6a50131bd5d8f79d9958d68
Korvo-Iam <emilaubakirov@gmail.com>
2026-02-05 12:52:47 +0500

    125205022026

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'tasks'
 
 urlpatterns = [
+    path('test-rollbar/', views.test_rollbar_error),
     path('', views.TaskListView.as_view(), name='task_list'),
     path('create/', views.TaskCreateView.as_view(), name='task_create'),
     path('<int:pk>/', views.TaskDetailView.as_view(), name='task_detail'),
     path('<int:pk>/update/', views.TaskUpdateView.as_view(), name='task_update'),
     path('<int:pk>/delete/', views.TaskDeleteView.as_view(), name='task_delete'),
-    path('test-rollbar/', views.test_rollbar_error),
 ]
